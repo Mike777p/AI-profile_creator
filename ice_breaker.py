34a8da2ed79b9aae068d1aa0bfdebb9a572ee171
Mike777p <mjp7@duck.com>
2023-07-01 13:12:35 -0500

    Add langchain dependencies

---
+++ ice_breaker.py
@@ -0,0 +1,7 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+if __name__ == "__main__":
+    print("Hello Langchain")
+
