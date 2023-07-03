f0ee1b4852088d5a1ebdb175d0178795ae86a3d8
Mike777p <mjp7@duck.com>
2023-07-03 15:14:24 -0500

    Create and call function to scrap linkedin profile

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,112 @@
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from dotenv import load_dotenv
+import os
+
+# load environment variables from .env file
+load_dotenv()
+
+information = """
+Career
+Toggle Career subsection
+1991–1997: Early acting roles
+1998–2002: Move to Hollywood
+2003–2006: Action roles
+2007–2008: Focus on small-scale drama
+2009–2015: Return to action films
+2016–present: Love & Friendship and beyond
+Modelling
+Personal life
+Toggle Personal life subsection
+Relationships
+Personal and political beliefs
+Legal issues
+Philanthropy
+Filmography
+Toggle Filmography subsection
+Film
+Television
+Video games
+Stage
+Radio
+Awards and nominations
+References
+External links
+Kate Beckinsale
+
+Article
+Talk
+Read
+View source
+View history
+
+Tools
+This is a good article. Click here for more information.
+Page semi-protected
+From Wikipedia, the free encyclopedia
+Kate Beckinsale
+Kate Beckinsale 2011 Comic-Con (truer color).jpg
+Beckinsale at the 2011 San Diego Comic-Con
+Born	Kathrin Romany Beckinsale
+26 July 1973 (age 49)
+London, England
+Alma mater	New College, Oxford
+Occupation	Actress
+Years active	1991–present
+Spouse	Len Wiseman
+​
+​(m. 2004; div. 2019)​
+Partner(s)	Michael Sheen
+(1995–2003)
+Children	1
+Parents	
+Judy Loe
+Richard Beckinsale
+Relatives	
+Samantha Beckinsale (half-sister)
+Roy Battersby (stepfather)
+Kathrin Romany Beckinsale (born 26 July 1973)[1][2][3] is an English actress. After some minor television roles, her film debut was Much Ado About Nothing (1993) while a student at the University of Oxford. She appeared in British costume dramas such as Prince of Jutland (1994), Cold Comfort Farm (1995), Emma (1996), Shooting Fish (1997) (a contemporary romantic crime comedy) and The Golden Bowl (2000), in addition to various stage and radio productions.
+
+Beckinsale started film work in the United States in the late 1990s. After appearing in small-scale dramas The Last Days of Disco (1998) and Brokedown Palace (1999), she starred in the war drama Pearl Harbor (2001), the romantic comedy Serendipity (2001), and Tiptoes (2003). She followed those with starring roles in The Aviator (2004) and Click (2006). All these films established her as a protagonist in Hollywood.
+
+Since playing the role of Selene in the Underworld film series (2003–2016), Beckinsale has become known for her work in action films, including Van Helsing (2004), Whiteout (2009), Contraband (2012), and Total Recall (2012). She continues to make appearances in smaller dramatic projects such as Snow Angels (2007), Nothing but the Truth (2008), and Everybody's Fine (2009). For the two first films she received positive reviews. In 2016, she received critical acclaim for her performance in the period comedy film Love & Friendship, for which she received a nomination for the Critics' Choice Movie Award for Best Actress in a Comedy. She returned in action films with Jolt (2021). She also starred in two television projects with The Widow (2019) and Guilty Party (2021).
+
+Early life and education
+Kathrin Romany Beckinsale was born on 26 July 1973 in the Chiswick district of London,[4][5] the only child of actors Richard Beckinsale and Judy Loe.[6] She has a half-sister from her father's earlier marriage, actress Samantha Beckinsale.[6] Her father was partly of Burmese descent.[7][8] Her parents did not marry until 1977, prior to Beckinsale starting nursery school,[9] when she made her first television appearance at age four, in an episode of This Is Your Life dedicated to her father.[10] When she was five, her father died suddenly of a heart attack aged 31; she was deeply traumatised by the loss and "started expecting bad things to happen."[11][6] Her widowed mother moved in with director Roy Battersby when Beckinsale was nine, and she was brought up alongside his four sons and daughter.[12] She has a close relationship with her stepfather,[6] who was a member of the Workers Revolutionary Party during her youth.[13] She helped to sell The News Line, a Trotskyist newspaper, as a little girl and has said the household phone was tapped following Battersby's blacklisting by the BBC.[13] Family friends included Ken Loach and Vanessa Redgrave.[13]
+
+Beckinsale was educated at Godolphin and Latymer School, an independent school for girls in Hammersmith, West London, and was involved with the Orange Tree Youth Theatre.[14] She was twice a winner of the WH Smith Young Writers Award for both fiction and poetry.[15] She has described herself as a "late bloomer": "All of my friends were kissing boys and drinking cider way before me. I found it really depressing that we weren't making camp fires and everyone was doing stuff like that."[16] She had a nervous breakdown and developed anorexia at age fifteen,[17] and underwent Freudian psychoanalysis for four years.[6]
+
+Beckinsale read French and Russian literature at New College, Oxford, and was later described by her contemporary Victoria Coren Mitchell, as "whip-clever, slightly nuts, and very charming".[18] She became friends with Roy Kinnear's daughter Kirsty.[19] She was involved with the Oxford University Dramatic Society, most notably being directed by fellow student Tom Hooper in a production of A View from the Bridge at the Oxford Playhouse.[20] As a Modern Languages student, she was required to spend her third year abroad, and studied in Paris. She then quit university to focus on her burgeoning acting career: "It was getting to the point where I wasn't enjoying either thing enough because both were very high pressure."[6] Beckinsale has stated she would like to complete her studies at Oxford University.[21][22]
+
+Career
+1991–1997: Early acting roles
+Beckinsale decided at a young age she wanted to be an actress: "I grew up immersed in film. My family were in the business. I quickly realised that my parents seemed to have much more fun in their work than any of my friends' parents."[23] She was inspired by the performances of Jeanne Moreau.[24] She made her television debut in 1991 with a small part in an ITV adaptation of P. D. James' Devices and Desires.[25] In 1992, she starred alongside Christopher Eccleston in Rachel's Dream, a 30‑minute Channel 4 short,[26] and in 1993, she appeared in the pilot of the ITV detective series, Anna Lee, starring Imogen Stubbs.[27]
+
+In 1993, Beckinsale landed the role of Hero in Kenneth Branagh's big-screen adaptation of Much Ado About Nothing. It was filmed in Tuscany, Italy, during a summer holiday from Oxford University.[28] She attended the film's Cannes Film Festival premiere and remembered it as an overwhelming experience. "Nobody even told me I could bring a friend!"[16] "I had Doc Martens boots on, and I think I put the flower from the breakfast tray in my hair."[29] Peter Travers of Rolling Stone was won over by her "lovely" performance[30] while Vincent Canby of The New York Times noted that she and Robert Sean Leonard "look right and behave with a certain naive sincerity, although they often seem numb with surprise at hearing the complex locutions they speak."[31] The film grossed over $22 million at the box office.[32] She made three other films while at university. In 1994, she appeared as Christian Bale's love interest in Prince of Jutland, a film based on the Danish legend which inspired Shakespeare's Hamlet,[33] and starred in the murder mystery Uncovered.[34] In 1995, while studying in Paris, she filmed the French language Marie-Louise ou la Permission.[35]
+
+Shortly after leaving Oxford University in 1995, Beckinsale starred in Cold Comfort Farm, as Flora Poste, a newly orphaned 1930s socialite sent to live with distant family members in rural England. The John Schlesinger-directed film was an adaptation of Stella Gibbons's novel and also featured Joanna Lumley, Eileen Atkins, Ian McKellen, Rufus Sewell and Stephen Fry. Beckinsale was initially considered too young, but was cast after she wrote a pleading letter to the director.[36] Emanuel Levy of Variety was reminded of "the strength of a young Glenda Jackson and the charm of a young Julie Christie."[37] Kevin Thomas of the Los Angeles Times classed the actress as "yet another of those effortlessly skilled British beauties who light up the screen."[38] Janet Maslin of The New York Times felt she played the role "with the perfect snippy aplomb."[39] The film grossed over $5 million at the US box office.[40] Also in 1995, she appeared in Haunted, a ghost story in which Derek Elley of Variety felt she "holds the screen, with both physical looks and verbal poise."[41] 1995 also saw Beckinsale's first professional stage appearance as Nina in The Seagull at Theatre Royal, Bath. She became romantically involved with co-star Michael Sheen after meeting during play rehearsals.[42] She later said: "I was all revved up to feel very intimidated. It was my first-ever play and my mother had cut out reviews of him in previous productions. And then he walked in ... It was almost like, 'God, well, I'm finished now. That's it, then.'... He's the most outrageously talented person I've ever met."[43] Irving Wardle of The Independent felt that "the casting, including Michael Sheen's volcanic Kostya and Kate Beckinsale's steadily freezing Nina, is mainly spot-on."[44] In early 1996, she starred in two further plays; Sweetheart at the Royal Court Theatre[45] and Clocks and Whistles at the Bush Theatre.[46]"""
 
 if __name__ == "__main__":
     print("Hello Langchain")
+    
+    openai_api_key = os.getenv('OPEN_API_KEY')
+    
+    
+    
+    broad_template = """
+    Given the linkedin information {information}. I want you to create:
+    1. a short summary
+    2. Two interesting facts about them
+    3. What is the best thing to say to them if I ever met them
+    """
 
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=broad_template
+    )
+    
+    llm = ChatOpenAI(temperature=1, model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+    
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    
+    print(chain.run(information=information))
---
+++ third_parties/edenMarco.py
@@ -0,0 +1,167 @@
+eden = {"public_identifier": "eden-marco",
+	"profile_pic_url": "https://s3.us-west-000.backblazeb2.com/proxycurl/person/eden-marco/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230703%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230703T195702Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=e68533800f6db3dcecabfe23a54a8f868cb1f90cebd173734da6c8e986b7e18e",
+	"first_name": "Eden",
+	"last_name": "Marco",
+	"full_name": "Eden Marco",
+	"occupation": "Customer Engineer at Google",
+	"headline": "Customer Engineer @ Google Cloud | Best-selling Udemy Instructor | Backend dev with passion for GenAI",
+	"summary": "Backend developer, Udemy.com best seller instructor\n",
+	"country": "IL",
+	"country_full_name": "Israel",
+	"experiences": [{
+		"starts_at": {
+			"day": 1,
+			"month": 6,
+			"year": 2022
+		},
+		"ends_at": None,
+		"company": "Google",
+		"company_linkedin_profile_url": "https://www.linkedin.com/company/google/",
+		"title": "Customer Engineer",
+		"description": None,
+		"location": "Tel Aviv, Israel",
+		"logo_url": "https://s3.us-west-000.backblazeb2.com/proxycurl/company/google/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230703%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230703T195707Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=a854619199b1c1533e68dcaddd86f1233cb03fc0ca1b892f019d40717e81c1e5"
+	}, {
+		"starts_at": {
+			"day": 1,
+			"month": 2,
+			"year": 2017
+		},
+		"ends_at": None,
+		"company": "Udemy",
+		"company_linkedin_profile_url": "https://www.linkedin.com/company/udemy/",
+		"title": "Independent Udemy Instructor",
+		"description": "Production and publication of three best-selling courses on the Udemy platform, with a total of 15k+ enrolled students, 1700+ ratings and a solid 4.7 ★ rating.",
+		"location": None,
+		"logo_url": "https://s3.us-west-000.backblazeb2.com/proxycurl/company/udemy/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230703%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230703T195707Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=3360bfe13c0cb0a56acb07661d34c60e04e6788ff07fd3232774bb86095a9987"
+	}, {
+		"starts_at": {
+			"day": 1,
+			"month": 4,
+			"year": 2020
+		},
+		"ends_at": {
+			"day": 1,
+			"month": 6,
+			"year": 2022
+		},
+		"company": "Orca Security",
+		"company_linkedin_profile_url": "https://www.linkedin.com/company/orca-security/",
+		"title": "Software Engineer",
+		"description": None,
+		"location": "Tel Aviv, Israel",
+		"logo_url": "https://s3.us-west-000.backblazeb2.com/proxycurl/company/orca-security/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230703%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230703T195707Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=0f613c82590b3aa970a771f44e8b9c1e67ff2731df71daae7d6d95174d032811"
+	}, {
+		"starts_at": {
+			"day": 1,
+			"month": 12,
+			"year": 2019
+		},
+		"ends_at": {
+			"day": 1,
+			"month": 4,
+			"year": 2020
+		},
+		"company": "Wizer",
+		"company_linkedin_profile_url": "https://www.linkedin.com/company/get-wizer/",
+		"title": "Software Engineer",
+		"description": None,
+		"location": None,
+		"logo_url": "https://s3.us-west-000.backblazeb2.com/proxycurl/company/get-wizer/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230703%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230703T195707Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=c3c524be021606d0b7b8a4b0fa055a437c35d51918e00f5aebe88dc994f69822"
+	}, {
+		"starts_at": {
+			"day": 1,
+			"month": 7,
+			"year": 2019
+		},
+		"ends_at": {
+			"day": 1,
+			"month": 12,
+			"year": 2019
+		},
+		"company": "Deep Instinct",
+		"company_linkedin_profile_url": "https://www.linkedin.com/company/deep-instinct/",
+		"title": "Software Engineer",
+		"description": None,
+		"location": "Tel Aviv Area, Israel",
+		"logo_url": "https://s3.us-west-000.backblazeb2.com/proxycurl/company/deep-instinct/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230703%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230703T195707Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=8537b58b1fc72085ee601486cfbee2574ed1491a54368e95558e93a2fc950f70"
+	}, {
+		"starts_at": {
+			"day": 1,
+			"month": 10,
+			"year": 2018
+		},
+		"ends_at": {
+			"day": 1,
+			"month": 7,
+			"year": 2019
+		},
+		"company": "Reichman University (IDC Herzliya)",
+		"company_linkedin_profile_url": "https://www.linkedin.com/school/reichman-university/",
+		"title": "Computer Science Teaching Assistant (External Lecturer)",
+		"description": "* Functional Programming (Including international school)\n* Introduction to Computer Science",
+		"location": "Herzliya Area, Israel",
+		"logo_url": "https://media.licdn.com/dms/image/C4D0BAQGg9wJflAQnjw/company-logo_400_400/0/1632054179547?e=1695859200&v=beta&t=DrpUjSemY_FVbTAIimxk0a8fnQP_-I5-H90hAgNAVgA"
+	}, {
+		"starts_at": {
+			"day": 1,
+			"month": 7,
+			"year": 2010
+		},
+		"ends_at": {
+			"day": 1,
+			"month": 8,
+			"year": 2014
+		},
+		"company": "Israel Defense Forces",
+		"company_linkedin_profile_url": "https://www.linkedin.com/company/israeldefenseforces/",
+		"title": "Captain",
+		"description": None,
+		"location": None,
+		"logo_url": "https://s3.us-west-000.backblazeb2.com/proxycurl/company/israeldefenseforces/profile?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=0004d7f56a0400b0000000001%2F20230703%2Fus-west-000%2Fs3%2Faws4_request&X-Amz-Date=20230703T195707Z&X-Amz-Expires=1800&X-Amz-SignedHeaders=host&X-Amz-Signature=5f71b0b1b5cb287e912c52a19c64c610a1f5f5b187a0df2dbaddebfcf2434025"
+	}],
+	"education": [{
+		"starts_at": {
+			"day": 1,
+			"month": 1,
+			"year": 2015
+		},
+		"ends_at": {
+			"day": 1,
+			"month": 1,
+			"year": 2019
+		},
+		"field_of_study": "Computer Science",
+		"degree_name": "Bachelor’s Degree",
+		"school": "Technion - Israel Institute of Technology",
+		"school_linkedin_profile_url": None,
+		"description": None,
+		"logo_url": "https://media.licdn.com/dms/image/C4D0BAQG12formuFdJg/company-logo_400_400/0/1559649766994?e=1695859200&v=beta&t=5cv5AaEpQsIpFyEKq1S_lUpe3fLWnFcmgNbEzFw7J4Q",
+		"grade": None,
+		"activities_and_societies": None
+	}],
+	"similarly_named_profiles": [{
+		"name": "Blumeden Marco Pasquali",
+		"link": "https://it.linkedin.com/in/blumeden-marco-pasquali-44603287",
+		"summary": "--",
+		"location": "Italy"
+	}, {
+		"name": "Eden Marco",
+		"link": "https://www.linkedin.com/in/edenmarco",
+		"summary": "Incoming Deloitte Strategy Summer Scholar",
+		"location": "Chapel Hill, NC"
+	}, {
+		"name": "Eden Marco",
+		"link": "https://www.linkedin.com/in/eden-marco-8299301ba",
+		"summary": "Student at San Francisco State University",
+		"location": "San Francisco Bay Area"
+	}, {
+		"name": "Eden Marco",
+		"link": "https://www.linkedin.com/in/eden-marco-875211201",
+		"summary": "Incoming First Year Optometry Student at Massachusetts College of Pharmacy & Health Sciences",
+		"location": "Greater Boston"
+	}]
+}
+
+
+print(eden["first_name"])
